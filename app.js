def1c4ee921e9379dc25cb2c7ba21b10a84c2283
HeroicVillain <dragonock540@gmail.com>
2020-02-26 22:00:37 -0600

    add listening

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var msg = 'Hello World Test!';
-console.log(msg);
+var http = require('http');
+
+http.createServer((req, res) => {
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+  
+}).listen(8080);
